9ccbd2fb6093d0bd5a0143ce2304b346d479cb33
gauravp1996 <50083243+gauravp1996@users.noreply.github.com>
2024-01-29 01:40:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "myec2" {
   ami           = "ami-00952f27cf14db9cd" 
   instance_type = "t2.micro"
   tags = {
