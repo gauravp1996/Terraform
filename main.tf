3cebe8c3f0dc679fdfea4c1ef579cc502d5db551
gauravp1996 <50083243+gauravp1996@users.noreply.github.com>
2024-01-29 01:41:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
     region = "ap-south-1"  
+    access_key = "AKIAZRP2PKWWB2M7ECXP" 
+    secret_key = "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"
 }
 
 resource "aws_instance" "myec2" {
